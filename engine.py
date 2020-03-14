d537501143218f6ebb57b3a0e5c3d0db6e1bd3f1
loganpadon <logan.padon@gmail.com>
2020-03-13 22:11:35 -0500

    Finished the second tutorial and moving on to the third

--- engine.py
+++ engine.py
@@ -1,11 +1,19 @@
 import tcod as libtcod
 from entity import Entity
 from input_handlers import handle_keys
+from map_objects.game_map import GameMap
 from render_functions import render_all, clear_all
+
 
 def main():
     screen_width = 80
     screen_height = 50
+    map_width = 80
+    map_height = 45
+    colors = {
+        'dark_wall': libtcod.Color(0, 0, 100),
+        'dark_ground': libtcod.Color(50, 50, 150)
+    }
 
     player = Entity(int(screen_width / 2), int(screen_height / 2), '@', libtcod.white)
     npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), '@', libtcod.yellow)
@@ -17,13 +25,15 @@ def main():
 
     con = libtcod.console_new(screen_width, screen_height)
 
+    game_map = GameMap(map_width, map_height)
+
     key = libtcod.Key()
     mouse = libtcod.Mouse()
 
     while not libtcod.console_is_window_closed():
         libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
 
-        render_all(con, entities, screen_width, screen_height)
+        render_all(con, entities, game_map, screen_width, screen_height, colors)
 
         libtcod.console_flush()
 
@@ -37,7 +47,8 @@ def main():
 
         if move:
             dx, dy = move
-            player.move(dx, dy)
+            if not game_map.is_blocked(player.x + dx, player.y + dy):
+                player.move(dx, dy)
 
         if exit:
             return True
@@ -47,4 +58,4 @@ def main():
 
 
 if __name__ == '__main__':
-     main()
+    main()
